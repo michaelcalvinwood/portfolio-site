9be199b288a0414df29637f78f4b8bde390e3b4c
Michael Wood <michaelwood33311@icloud.com>
2023-05-20 16:52:21 -0400

    added first animated social icon

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import HeroImage from '../assets/webdev-michael-wood.jpeg';
-import { HiArrowRight } from 'react-icons/hi';
+import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
 
 const Home = () => {
   return (
-    <div name='home' className='h-screen w-full bg-gradient-to-b from-black to-gray-800'>   
+    <div name='home' className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>   
         <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
             <div className='flex flex-col justify-center h-full '>
                 <h2 className='text-4xl sm:text-7xl font-bold text-white'>
@@ -14,10 +14,10 @@ const Home = () => {
                     I have over 30 years of experience building and designing software, hardware, servers, networks, and more. Currently, I love working on full-stack websites and apps using Nodejs, React, Redux, NextJs, PHP, WordPress, HTML, CSS, SASS, TailWind, and Chakra-UI. 
                 </p>
                 <div>
-                    <button>
+                    <button className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
                         Portfolio
-                        <span>
-                            <HiArrowRight />
+                        <span className='group-hover:rotate-90 duration-300' >
+                            <MdOutlineKeyboardArrowRight size={20} className='ml-2'/>
                         </span>
                     </button>
                 </div>
---
+++ src/components/SocialLinks.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { HiOutlineMail } from 'react-icons/hi';
+import { BsFillPersonLinesFill}  from 'react-icons/bs';
+
+const SocialLinks = () => {
+  return (
+    <div className='flex flex-col top-[35%] left-0 fixed'>
+        <ul>
+            <li className='flex justify-between items-center w-40 h-14 px-4 bg-gray-500 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 cursor-pointer'>
+                <a href="" className='flex justify-between items-center w-full text-white'>
+                    <>LinkedIn <FaLinkedin size={30}/></>
+                </a>
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default SocialLinks
