fce808958fe665ee51c766015d5182957956e231
Michael Wood <michaelwood33311@icloud.com>
2023-05-20 14:19:49 -0400

    complete navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaBars, FaTimes } from 'react-icons/fa';
 
 const NavBar = () => {
+    const [nav, setNav] = useState(false);
+
     const links = [
         {
             id: 1,
@@ -29,10 +31,20 @@ const NavBar = () => {
        <div>
          <h1 className='text-5xl font-signature ml-2'>Michael</h1>
        </div>
-       <ul className='flex'>
+       <ul className='hidden md:flex'>
             {links.map(link => <li key={link.id} className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200'>{link.link}</li>)}
-
        </ul>
+      
+       <div  className='cursor-pointer pr-4 z-10 text-gray-500 md:hidden' onClick={() => setNav(!nav)}>
+        {nav ? <FaTimes size={30} /> : <FaBars size={30}/>}
+       </div>
+       { nav && (
+        <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500'>
+        {links.map(link => <li key={link.id} className='px-4 capitalize cursor-pointer py-6 text-4xl'>{link.link}</li>)}
+        
+        </ul>
+       )}
+      
     </div>
   )
 }
