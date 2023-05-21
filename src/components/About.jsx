326c99d19e4cdd2f2bf003b05439fa3c5760bcdd
Michael Wood <michaelwood33311@icloud.com>
2023-05-21 10:28:43 -0400

    added place-holder portfolio

---
+++ src/components/About.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center h-full w-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+            </div>
+            <p className='text-xl mt-5 '>
+                My love for programming began with the very first computers — with my starting programming languages being Assembler, Pascal, Fortran, and BASIC — as well as Linux network programming in C. Over the decades, I passionately devoured new languages and frameworks; keeping my skills current through continuing education, including obtaining a full-stack Web Development diploma from Brainstation.
+            </p>
+            <br />
+            <p className='text-xl'>
+                I'm now proficient in a variety of programming paradigms and languages, and I'm equally comfortable with a variety of databases such as SQL, MongoDb, Redis, and even newer AI vector databases such as Qdrant. I'm also equally comfortable with frontend and backend development. My current employment involves a combination of Nodejs, React, Redux, PHP, WordPress, HTML, CSS, and Nextjs.
+            </p>
+            <br />
+            <p className='text-xl'>
+                In short, I have no preference for any programming language, paradigm, nor environment. I strongly believe in selecting whatever provides the best solution to meet current requirements. I happily adapt — always striving to provide the best possible solution in the shortest amount of time.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default About
--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -32,7 +32,7 @@ const SocialLinks = () => {
         },
     ]
   return (
-    <div className='flex flex-col top-[35%] left-0 fixed'>
+    <div className='hidden lg:flex flex-col top-[35%] left-0 fixed'>
         <ul>
             {links.map(({id, child, href, style, download}) => {
                   return <li key={id} className={'flex justify-between items-center w-40 h-14 px-4 bg-gray-500 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 cursor-pointer ' + style} >
