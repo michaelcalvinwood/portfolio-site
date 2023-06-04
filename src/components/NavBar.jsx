6493624d98475fa380b38451e5aeb587b691e6eb
Michael Wood <michaelwood33311@icloud.com>
2023-06-04 16:58:39 -0400

    fixed mobile styling issues

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -43,7 +43,7 @@ const NavBar = () => {
        </div>
        { nav && (
         <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500'>
-        {links.map(link => <li key={link.id} className='px-4 capitalize cursor-pointer py-6 text-4xl'>{link.link}</li>)}
+        {links.map(link => <li key={link.id} className='px-4 capitalize cursor-pointer py-6 text-4xl'><Link onClick={()=>setNav(false)} to={link.link} smooth={true} duration={500}>{link.link}</Link></li>)}
         
         </ul>
        )}
