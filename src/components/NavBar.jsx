56de9db8fe7faf991a31b4863a78d1a18a85f267
Michael Wood <michaelwood33311@icloud.com>
2023-09-21 10:29:31 -0400

    added recommendations to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,6 +4,7 @@ import { Link }  from 'react-scroll';
 const NavBar = () => {
     const [nav, setNav] = useState(false);
 
+    //https://michaelcalvinwood.net/docs/Michael_Recommendation_PYMNTS.pdf
     const links = [
         {
             id: 1,
@@ -27,6 +28,11 @@ const NavBar = () => {
         },
         {
             id: 6,
+            link: 'recommendations',
+            href: 'https://michaelcalvinwood.net/docs/Michael_Recommendation_PYMNTS.pdf'
+        },
+        {
+            id: 7,
             link: 'contact'
         },
     ]
@@ -35,19 +41,24 @@ const NavBar = () => {
        <div>
          <h1 className='text-5xl font-signature ml-2'>Michael</h1>
        </div>
-       <ul className='hidden md:flex'>
+       <ul className='hidden lg:flex'>
             {links.map(link => (
             <li key={link.id} className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200'>
-                <Link to={link.link.replaceAll(' ', '')} smooth={true} duration={500}>{link.link}</Link>
+                {!link.href && <Link to={link.link.replaceAll(' ', '')} smooth={true} duration={500}>{link.link}</Link>}
+                {link.href && <a href={link.href} target="_blank">{link.link}</a>}
             </li>))}
        </ul>
       
-       <div  className='cursor-pointer pr-4 z-10 text-gray-500 md:hidden' onClick={() => setNav(!nav)}>
+       <div  className='cursor-pointer pr-4 z-10 text-gray-500 lg:hidden' onClick={() => setNav(!nav)}>
         {nav ? <FaTimes size={30} /> : <FaBars size={30}/>}
        </div>
        { nav && (
         <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500'>
-        {links.map(link => <li key={link.id} className='px-4 capitalize cursor-pointer py-6 text-4xl'><Link onClick={()=>setNav(false)} to={link.link.replaceAll(' ', '')} smooth={true} duration={500}>{link.link}</Link></li>)}
+        {links.map(link => <li key={link.id} className='px-4 capitalize cursor-pointer py-6 text-4xl'>
+                {!link.href && <Link onClick={()=>setNav(false)} to={link.link.replaceAll(' ', '')} smooth={true} duration={500}>{link.link}</Link>}
+                {link.href && <a href={link.href} target="_blank">{link.link}</a>}
+            </li>)
+        }
         
         </ul>
        )}
