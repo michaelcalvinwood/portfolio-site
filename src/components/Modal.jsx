5bb14eb7174fabab6aba99e9fa4295f851012216
Michael Wood <michaelwood33311@icloud.com>
2023-10-14 09:22:55 -0400

    added NLP Library expertise

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,14 +3,14 @@ import React, { useEffect } from 'react'
 function Modal({useModal, setUseModal}) {
     
     useEffect(() => {
-        setTimeout(() => setUseModal(false), 4000)
+        setTimeout(() => setUseModal(false), 5000)
     })
   return (
     <div>
         {useModal && <div id="modal" className='text-red-600 flex justify-center items-center fixed top-0 left-0 w-screen h-screen'>
             <div id="modalText" className='p-8 bg-gradient-to-b from-black to-gray-800 text-white rounded-lg border border-white'>
                 <p className='text-center text-lg'><b>Site Under Construction</b></p>
-                <p className='pt-4 text-center'>Full site links coming November 1, 2023.</p>
+                <p className='pt-4 text-center'>Coming November 1, 2023.</p>
             </div>
         </div>}
     </div>
