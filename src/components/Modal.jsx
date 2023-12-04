940f204facecde80eab3359e5567b6df49a121fb
Michael Wood <michaelwood33311@icloud.com>
2023-12-04 07:09:27 -0500

    altered availability date

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -10,7 +10,7 @@ function Modal({useModal, setUseModal}) {
         {useModal && <div id="modal" className='text-red-600 flex justify-center items-center fixed top-0 left-0 w-screen h-screen'>
             <div id="modalText" className='p-8 bg-gradient-to-b from-black to-gray-800 text-white rounded-lg border border-white'>
                 <p className='text-center text-lg'><b>Site Under Construction</b></p>
-                <p className='pt-4 text-center'>Coming January 1, 2024.</p>
+                <p className='pt-4 text-center'>Coming January 2, 2024.</p>
             </div>
         </div>}
     </div>
